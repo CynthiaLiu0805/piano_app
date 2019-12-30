b340283350c11551c3dffdb2afa1414b087e94bb
liuy363 <liuy363@mcmaster.ca>
2019-12-29 19:17:53 -0500

    Initial Commit

---
+++ piano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  piano
+//
+//  Created by Cynthia on 29/12/2019.
+//  Copyright © 2019 Cynthia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
